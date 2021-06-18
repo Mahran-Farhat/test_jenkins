cb96dff9af9099d8109df702aa4f25ec6e6af491
Mahran-Farhat <farhatmahran@gmail.com>
2021-06-19 00:19:59 +0100

    second commit

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
 public static void main(String args []){
 
 for(int i = 0; i<= 10; i++)
-System.out.println(i+"Hello in test jenkins");
+System.out.println(i+" Hello in test jenkins");
 
 }
 
